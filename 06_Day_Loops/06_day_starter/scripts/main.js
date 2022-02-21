fa74ded698d6d6e2fd0514a5bc6947acf49db212
Michael Godfrey <m.godfrey59@gmail.com>
2022-02-21 21:59:24 +1100

    "practiced loops and functions"

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -48,6 +48,7 @@ for (i = 0; i < 101; i++) {
 
 console.log(`the sum of the numbers from 0 to 100 is ${sum}`);
 
+// sum odd and even numbers separately
 let evenSum = 0;
 let oddSum = 0;
 for (i = 0; i < 101; i++) {
@@ -56,3 +57,97 @@ for (i = 0; i < 101; i++) {
 
 console.log(evenSum);
 console.log(oddSum);
+
+// print results in an array
+const results = [];
+results.push(evenSum, oddSum);
+console.log(results);
+// gerate 5 random numbers
+for (i = 0; i < 5; i++) {
+  console.log(Math.floor(Math.random() * 9999999))
+}
+// generate random characters
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+randomString = '';
+for (i = 0; i < 10; i++) {
+  randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+}
+console.log(randomString);
+// generate random characters at random numbers of characters
+randomVariableString = '';
+randomLength = Math.floor(Math.random() * (25 - 10) + 10);
+for (i = 0; i < randomLength; i++) {
+  randomVariableString += characters.charAt(Math.floor(Math.random() * characters.length));
+}
+console.log(randomVariableString);
+
+// generate a hexidecimal numbers
+const hexChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+let hexidecimal = '';
+for (i = 0; i < 6; i++) {
+  hexidecimal += hexChars.charAt(Math.floor(Math.random() * hexChars.length));
+}
+console.log(hexidecimal);
+
+// print as new array in capital letters
+const places = [
+  'Albania', 'Bolivia', 'Canada', 'Denmark', 'Ethiopia', 'Finland', 'Germany', 'Hungary', 'Ireland', 'Japan', 'Kenya'
+];
+let uppercase = [];
+for (i = 0; i < places.length; i++) {
+  uppercase.push(places[i].toUpperCase());
+}
+console.log(uppercase);
+// print array of countries lengths
+let lengths = [];
+for (i = 0; i < places.length; i++) {
+  lengths.push(places[i].length);
+}
+console.log(lengths);
+// create new full on array with above info
+let superArray = [];
+for (i = 0; i < places.length; i++) {
+  let array  = [];
+  array.push(places[i], places[i].substr(0,3).toUpperCase(), places[i].length);
+  superArray.push(array);
+}
+console.log(superArray);
+
+// In above countries array, check if there is a country or countries containing the word 'land'.
+
+let containsLand = [];
+for (i = 0; i < places.length; i++) {
+  if (places[i].endsWith('land')) {
+    containsLand.push(places[i]);
+  }
+}
+console.log(containsLand);
+
+// country with most characters in name
+let maxChar = '';
+for (i = 0; i < places.length; i++) {
+
+}
+
+console.log(maxChar);
+
+// countries with 5 characters in name
+let fiveChars = [];
+for (i = 0; i < places.length; i++) {
+  if (places[i].length === 5) {
+    fiveChars.push(places[i]);
+  }
+}
+
+console.log(fiveChars)
+
+// acronym
+
+const apps = ["MongoDB", "Express", "React", "Node"];
+let acronym = '';
+
+for (const app of apps) {
+  acronym = app[0].toString();
+}
+
+console.log(acronym);
--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,51 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+function fullName(firstName, lastName) {
+  console.log(`${firstName} ${lastName}`);
+}
+
+fullName("Michael", "Godfrey");
+
+function addNumbers(a, b) {
+  let sum = a + b;
+  console.log(sum);
+  return sum;
+}
+
+addNumbers(4, 7);
+
+function findMax(a, b, c) {
+  let max;
+  if (a > b &&  a && c) {
+    max = a;
+  } else if (b > a && b > c) {
+    max = b;
+  } else if (c > a && c > b) {
+    max =  c;
+  } else {
+    console.log("it's a draw");
+  }
+  console.log(max);
+}
+
+findMax(1,1,0);
+
+const places = [
+  'Albania', 'Bolivia', 'Canada', 'Denmark', 'Ethiopia', 'Finland', 'Germany', 'Hungary', 'Ireland', 'Japan', 'Kenya'
+];
+
+function printArray (arr) {
+  for (i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printArray(places);
+
+function showDateTime() {
+  const now = new Date();
+  let day = now.getDate();
+  let month = now.getMonth() + 1;
+  let year = now.getFullYear();
+  console.log(`${day}/${month}/${year}`);
+}
+
+showDateTime();
