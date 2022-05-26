abfa112f99022daf3085cbad0f16fb69222861ae
Michael Godfrey <m.godfrey59@gmail.com>
2022-05-26 21:33:01 +1000

    completed arrays

--- 12_Day_Regular_expressions/12_day_starter/scripts/main.js
+++ 12_Day_Regular_expressions/12_day_starter/scripts/main.js
@@ -1,2 +1,17 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// alert('Open the console and check if the countries has been loaded')
+
+//1
+const str1 = "He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month";
+
+const pattern1 = /\d+/g
+
+const matches1 = str1.match(pattern1)
+
+console.log(matches1)
+
+//2
+points = ["-1", "2", "-4", "-3", "-1", "0", "4", "8"];
+sortedPoints = [-4, -3, -1, -1, 0, 2, 4, 8];
+distance = 12;
+
