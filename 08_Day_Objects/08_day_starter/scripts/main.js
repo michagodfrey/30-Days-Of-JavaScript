a683fb19d7852390cbf54be1e198ef1014d1ddbe
Michael Godfrey <m.godfrey59@gmail.com>
2022-06-02 21:23:53 +1000

    Worked on Objects

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -68,10 +68,29 @@ for (user in users) {
 
 // 4. Set your name in the users object without modifying the original users object
 
+const newUser = {
+  Michael: {
+    email: 'mike@mike.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node'],
+    age: 39,
+    isLoggedIn: false,
+    points: 200
+  }
+}
 
+const newObject = Object.assign(newUser, users);
+console.log(newObject);
 
 // 5 Get all keys or properties of users object
+
+const keys = Object.keys(users)
+console.log(keys)
+
 // 6 Get all the values of users object
+
+const values = Object.values(users)
+console.log(values)
+
 // 7 Use the countries object to print a country name, capital, populations and languages.
 
 // for (country in countries) {
@@ -83,6 +102,11 @@ for (user in users) {
 
 // level 3
 
+// 1. Create an object literal called _personAccount_.
+//It has _firstName, lastName, incomes, expenses_ properties and it has _totalIncome,
+// totalExpense, accountInfo,addIncome, addExpense_ and _accountBalance_ methods.
+//Incomes is a set of incomes and its description and expenses is a set of incomes
+//and its description.
 const personAccount = {
   firstName: "",
   lastName: "",
@@ -96,7 +120,115 @@ const personAccount = {
   accountBalance: ""
 }
 
+// 2. **** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
 
+const currentUsers = [
+{
+    _id: 'ab12ex',
+    username: 'Alex',
+    email: 'alex@alex.com',
+    password: '123123',
+    createdAt:'08/01/2020 9:00 AM',
+    isLoggedIn: false
+},
+{
+    _id: 'fg12cy',
+    username: 'Asab',
+    email: 'asab@asab.com',
+    password: '123456',
+    createdAt:'08/01/2020 9:30 AM',
+    isLoggedIn: true
+},
+{
+    _id: 'zwf8md',
+    username: 'Brook',
+    email: 'brook@brook.com',
+    password: '123111',
+    createdAt:'08/01/2020 9:45 AM',
+    isLoggedIn: true
+},
+{
+    _id: 'eefamr',
+    username: 'Martha',
+    email: 'martha@martha.com',
+    password: '123222',
+    createdAt:'08/01/2020 9:50 AM',
+    isLoggedIn: false
+},
+{
+    _id: 'ghderc',
+    username: 'Thomas',
+    email: 'thomas@thomas.com',
+    password: '123333',
+    createdAt:'08/01/2020 10:00 AM',
+    isLoggedIn: false
+}
+];
+
+const products = [
+{
+_id: 'eedfcf',
+name: 'mobile phone',
+description: 'Huawei Honor',
+price: 200,
+ratings: [
+  { userId: 'fg12cy', rate: 5 },
+  { userId: 'zwf8md', rate: 4.5 }
+],
+likes: []
+},
+{
+_id: 'aegfal',
+name: 'Laptop',
+description: 'MacPro: System Darwin',
+price: 2500,
+ratings: [],
+likes: ['fg12cy']
+},
+{
+_id: 'hedfcg',
+name: 'TV',
+description: 'Smart TV:Procaster',
+price: 400,
+ratings: [{ userId: 'fg12cy', rate: 5 }],
+likes: ['fg12cy']
+}
+]
+
+// Imagine you are getting the above currentUsers collection from a MongoDB database.
+//   a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+//   b. Create a function called signIn which allows user to sign in to the application
+
+
+function signUp(newUser) {
+  for (user in currentUsers) {
+    if (currentUsers[user].username.toLowerCase() === newUser.toLowerCase())
+    console.log('That user already exists');
+  }
+}
+
+signUp('thomas');
+
+function signIn(email, password) {
+  for (user in currentUsers) {
+    if (currentUsers[user].email === email && currentUsers[user].password === password) {
+      console.log('Welcome! You are logged in')
+    } else {
+      console.log('Sorry, email and password combination not recognized.');
+    }
+  }
+}
+
+signIn('thomas@thomas.com', '123333')
+
+// 3. The products array has three elements and each of them has six properties.
+//   a. Create a function called rateProduct which rates the product
+//   b. Create a function called averageRating which calculate the average rating of a product
+
+
+
+
+// 4. Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.
 
 // function signUp() {
 //   let newUser = prompt('enter username');
